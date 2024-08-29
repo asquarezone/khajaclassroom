b9838501cc212e8f70700944d47fe278e3e895c7
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-08-29 09:26:59 +0530

    Added program for conditions

---
+++ python/Aug24/instruction_programs/hacker_rank_conditions.py
@@ -0,0 +1,21 @@
+"""This program is solution for Hacker Rank
+Problem of conditionals
+"""
+
+n = int(input().strip())
+# checking if the input is valid
+is_valid = 1 <= n <= 100
+if not is_valid:
+    exit(1)
+
+# if the n is valid
+is_even = n%2 == 0
+if not is_even :
+    print('Not Weird')
+else:
+    if 2 <= n <= 5:
+        print('Not Weird')
+    elif 6 <= n <= 20:
+        print ('Wierd')
+    else:
+        print('Not Weird')
