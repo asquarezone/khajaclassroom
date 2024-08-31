a43625feb264e04fb1323068a608d14f70569782
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-08-31 19:49:48 +0530

    Added intrest calculations

--- python/Aug24/instruction_programs/modules_demo/main.py
+++ python/Aug24/instruction_programs/modules_demo/main.py
@@ -3,5 +3,19 @@
 import utils
 
 
+def main():
+    """Main function
+    """
+    choice = 'y'
+    while choice == 'y':
+        principle = float(input('Enter principle amount: '))
+        rate = float(input('Enter rate of intrest: '))
+        time = float(input('Enter time period in years: '))
+        print(f'Compound Intrest: {utils.compound_intrest(principle, rate, time)}')
+        print(f'Simple Intrest: {utils.simple_intrest(principle, rate, time)}')
+        choice = input('Do you want to continue? (y/n): ').lower()
+
+
+
 if __name__ == '__main__':
-    print(utils.add(3,5))
+    main()
--- python/Aug24/instruction_programs/modules_demo/utils.py
+++ python/Aug24/instruction_programs/modules_demo/utils.py
@@ -9,3 +9,25 @@ def add(x,y):
         y (int): y
     """
     return x + y
+
+
+def simple_intrest(principle, rate, time):
+    """This function will calculate simple intrest
+
+    Args:
+        principle (float): principle amount
+        rate (float): rate of intrest
+        time (float): time period
+    """
+    return principle + (principle * rate * time)/100
+
+
+def compound_intrest(principle, rate, time):
+    """This function will calculate compound intrest
+
+    Args:
+        principle (float): principle amount
+        rate (float): rate of intrest
+        time (float): time period
+    """
+    return principle * (1 + rate/100)**time
