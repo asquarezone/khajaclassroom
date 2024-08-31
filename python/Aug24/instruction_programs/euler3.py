5a8f334e2a02ddaf51d5d8af73a812743f4eac85
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-08-31 09:58:56 +0530

    Added euler 3

--- python/Aug24/instruction_programs/euler3.py
+++ python/Aug24/instruction_programs/euler3.py
@@ -1,28 +1,53 @@
 """This program demonstrates the solution of 
 project euler problem 3
 
-concepts:
-  while
-  if
-  variables
 """
 
-number = int(input("Enter the number: "))
-index = number // 2
-while index >= 2:
-    print(index)
-    if number % index == 0:
-        # we need to check if index i prime or not
-        print("found factor")
-        counter = 2
-        is_prime = True
-        while counter <= index // 2:
-            if index % counter == 0:
-                is_prime = False
-                break
-            counter += 1
-        if is_prime:
-            print("Largest Prime factor is ")
-            print(index)
-            break
-    index -= 1
+def is_prime(number):
+    """This function will check if the number is prime or not
+
+    Args:
+        number (int): number passed
+
+    Returns:
+        bool: True if prime, False otherwise      
+    """
+    for index in range(2, number // 2 + 1):
+        if number % index == 0:
+            return False
+    return True
+
+def factors(number):
+    """This function will find the factors of a number
+
+    Args:
+        number (int): number passed
+    
+    Returns:
+       Tuple: factors
+    """
+    factors_list = []
+    for index in range(2, number//2 + 1):
+        if number % index == 0:
+            factors_list.append(number)
+    return tuple(factors_list)
+
+
+# Lets find the largest factor
+def largest_prime_factor(number):
+    """This function will find the largest prime factor of a number
+
+    Args:
+        number (int): number to be checked for
+
+    Returns:
+        int: largest prime factor
+    """
+    for index in range(number//2 , 1, -1):
+        if number%index == 0 and is_prime(index):
+            return index
+    return None
+
+
+print(largest_prime_factor(13195))
+
