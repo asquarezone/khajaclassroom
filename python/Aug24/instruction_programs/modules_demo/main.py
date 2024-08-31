ddb75b8100e40964e4fd230375218fa9ce950d5a
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-08-31 19:01:35 +0530

    Added executable and helper functions

---
+++ python/Aug24/instruction_programs/modules_demo/main.py
@@ -0,0 +1,7 @@
+"""This module will act as executable
+"""
+import utils
+
+
+if __name__ == '__main__':
+    print(utils.add(3,5))
---
+++ python/Aug24/instruction_programs/modules_demo/utils.py
@@ -0,0 +1,11 @@
+"""This module will contain reusable(helper) functions
+"""
+
+def add(x,y):
+    """This function will add two numbers
+
+    Args:
+        x (int): x
+        y (int): y
+    """
+    return x + y
