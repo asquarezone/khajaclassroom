28e3db5ce8d3935e02af98ce1726b954b028834f
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-09-09 10:03:48 +0530

    Added encrypted implementation

--- python/Aug24/cli/PasswordManager/store.py
+++ python/Aug24/cli/PasswordManager/store.py
@@ -1,9 +1,19 @@
 """This module will store the passwords to a file
 """
 import os
+import json
 from cryptography.fernet import Fernet
 
 PASSWORD_FILE = 'passwords.secret'
+
+def load_secrets():
+    """load existing secrets
+    """
+    with open(PASSWORD_FILE, mode='r', encoding='utf16') as file:
+        return json.load(file)
+
+
+secrets_dictionary = load_secrets()
 
 
 def load_key(key_file="passwords.key"):
@@ -28,6 +38,23 @@ def get_key(key_file="passwords.key"):
     key = load_key(key_file)
     return key
 
+def encrypt(plain_text):
+    """This method encyrpts text
+    """
+    key = get_key()
+    f = Fernet(key)
+    cipher_bytes = f.encrypt(plain_text.encode())
+    cipher_text = cipher_bytes.decode('utf16')
+    return cipher_text
+
+def decrypt(cipher_text):
+    """This method decrypts text
+    """
+    key = get_key()
+    f = Fernet(key)
+    cipher_bytes = cipher_text.encode('utf16')
+    return f.decrypt(cipher_bytes).decode()
+
 
 def save_password(service, username, password):
     """This function will save passwords to a file
@@ -37,10 +64,12 @@ def save_password(service, username, password):
         username (str): username
         password (str): password
     """
-
-    with open(PASSWORD_FILE,mode='a',encoding='utf8') as file:
-        file.write(f"{service}\t{username}\t{password}\n")
-    # add encryption
+    secrets_dictionary[service] = {
+        'username': encrypt(username), 
+        'password': encrypt(password)
+    }
+    with open(PASSWORD_FILE, mode='w', encoding='utf16') as file:
+        json.dump(secrets_dictionary,file, indent=4)
 
 def retrieve_password(service_name):
     """This function will retrive the password based on service
@@ -53,12 +82,8 @@ def retrieve_password(service_name):
     Returns:
        tuple: (username, password)
     """
-    # add decryption
-    with open(PASSWORD_FILE,mode='r',encoding='utf8') as file:
-        for line in file.readlines():
-            service,username,password = line.split("\t")
-            if service == service_name:
-                return (username,password)
-    # asking stuff which is not present
-    return None
-    
+    if service_name not in secrets_dictionary:
+        raise ValueError('Invalid Service Name')
+    username = decrypt(secrets_dictionary[service_name]['username'])
+    password = decrypt(secrets_dictionary[service_name]['password'])
+    return (username, password)
