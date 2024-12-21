0119a2f0ec39270d03ca744671af8adc3c96061b
shaikkhajaibrahim <shaikkhajaibrahim@live.in>
2024-12-21 20:39:09 +0530

    Added files

--- python/Dec24/basics/studentdetails.py
+++ python/Dec24/basics/studentdetails.py
@@ -5,40 +5,47 @@ This module contains the necessary functions to store
 student details
 """
 
+from dataclasses import dataclass, asdict
+import json
+import os
 
+
+@dataclass
 class Student:
     """This class represents student"""
 
-    def __init__(self, name, course, email):
-        self.name = name
-        self.email = email
-        self.course = course
+    name: str
+    email: str
+    course: str
 
-    def __repr__(self):
-        return f"name = {self.name}, email = {self.email}, course = {self.course}"
 
 # This is memory of our application
 student_dict = dict()
+
+DATASTORE_PATH = "studentdetails.json"
 
 
 def add_student():
     """
     This function adds the student info
     """
+
     name = input("Enter student's name: ")
     course = input("Enter Course: ")
     email = input("Enter email id: ")
-    student = Student(name, course, email)
-    student_dict[email] = student
+    student = Student(name=name, email=email, course=course)
+    student_dict[email] = asdict(student)
+    with open(DATASTORE_PATH, mode="w", encoding="utf-8") as student_file:
+        json.dump(student_dict, student_file, indent=4)
 
 
 def fetch_student_details():
     """
     This function fetches the student details
     """
-    email = input("Enter the email id of the student")
+    email = input("Enter the email id of the student: ")
     if email in student_dict:
-        student = student_dict[email]
+        student = Student(**student_dict[email])
         print(f"name = {student.name}")
         print(f"email = {student.email}")
         print(f"course = {student.course}")
@@ -46,10 +53,20 @@ def fetch_student_details():
         print("student not found...")
 
 
+def initialize_data():
+    """This method will read all the data from
+    json file into student dictionary if the file exists
+    """
+    if os.path.exists(DATASTORE_PATH):
+        with open(DATASTORE_PATH, mode='r', encoding="utf-8") as students_file:
+            global student_dict 
+            student_dict = json.load(students_file)
+
 def menu():
     """
     This is menu for the application
     """
+    initialize_data()
     while True:
         choice = input(
             "Enter your choice: 1 for adding students and 2 for student info and 3 for exit: "
